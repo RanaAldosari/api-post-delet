3b3913deba0e6d4425e31b1339c746cc2ae1a5ec
Rana Aldosari <rana@example.com>
2025-05-12 13:55:45 +0300

    update-lab-2

--- main.js
+++ main.js
@@ -5,7 +5,39 @@ let image = document.getElementById("img");
 let containerUsers = document.getElementById("displayUsers");
 
 
+let nameError=document.getElementById("name-error")
+let textError=document.getElementById("text-error")
+let imgError=document.getElementById("img-error")
+
+
+
 btn.addEventListener("click", () => {
+if(username.value.length<4){
+  alert("username must be more then 4 charachters !")
+        return
+
+}
+if(text.value.length<6){
+  alert("your phraghraph mst be more than 6 characters !")
+return
+}
+if(image.src===""){
+alert("please upload your image!")
+return
+}
+
+fetch("https://68219a91259dad2655afc3cc.mockapi.io/api/users/user")
+.then(response => response.json())
+  .then(data => 
+  {
+    let userFind=data.find(user=>user.username ===username.value)
+    if(userFind){
+nameError.innerText="the username already exists"
+nameError.style.color="red"
+return;
+    }}
+  );
+
   fetch('https://68219a91259dad2655afc3cc.mockapi.io/api/users/user', {
     method: 'POST',
     body: JSON.stringify({
@@ -17,11 +49,20 @@ btn.addEventListener("click", () => {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
+  
     .then(response => response.json())
     .then(() => {
       allUsers();
     })
-
+// if(username.length<4){
+// nameError.innerText=`username must be more than 4 characters`
+// }
+// if(text.length<6){
+// textError.innerText=`your phragraph must be more than characters`
+// }
+// if(image===""){
+// imgError.innerText="must be uplad image"
+// }
 });
 
 function allUsers() {
@@ -67,5 +108,8 @@ btnDel.style.border="none"
       });
     });
 }
+
+
+
 
 window.onload = allUsers;
